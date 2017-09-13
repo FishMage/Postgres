694e52a64c413186e3ccc9ee4082a7f4a6709573
FishMage <schen358@wisc.edu>
2017-09-13 02:15:16 -0400

    update import with SQL

--- import.py
+++ import.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
+import psycopg2
 import xml.sax
+import psycopg2.extras
+
+con = None
+cur = None
 
 class DblpHandler(xml.sax.ContentHandler):
     def __init__(self):
@@ -7,7 +12,7 @@ class DblpHandler(xml.sax.ContentHandler):
         self.pubkey = ""
         self.title = ""
         self.journal = ""
-        self.year = ""
+        self.year = None  
         self.booktitle = ""
         self.author = ""
 
@@ -15,18 +20,38 @@ class DblpHandler(xml.sax.ContentHandler):
         self.CurrentData = tag
         if tag == "article":
             print "***Article***"
-            pubkey = attributes["key"]
-            print "Pubkey:",pubkey
+            self.pubkey = attributes["key"]
+            print "Pubkey:",self.pubkey
+            if self.CurrentData == "title":
+                print "Title: ",self.title
+            elif self.CurrentData =="journal":
+                print "Journal: ",self.journal
+            elif self.CurrentData == "year":
+                print "Year: ",self.year
+            elif self.CurrentData =="author":
+                print "Author: ",self.author 
+                #cur.execute("INSERT INTO authorship(pubkey, author) VALUES(%s, %s)", (pubkey , self.author))
+            #print pubkey, self.title, self.journal, self.year
+            cur.execute("INSERT INTO article(pubkey, title,journal, year) VALUES(%s,%s,%s,%s)", (self.pubkey,self.title, self.journal, self.year))
 
         if tag == "inproceedings":
             print "***inproceedings***"
-            pubkey = attributes["key"]
-            print "Pubkey:",pubkey
+            self.pubkey = attributes["key"]
+            print "Pubkey:",self.pubkey
+            if self.CurrentData == "title":
+                print "Title: ",self.title 
+            elif self.CurrentData == "year":
+                print "Year: ",self.year
+            elif self.CurrentData =="booktitle":
+                print "booktitle: ",self.booktitle
+            elif self.CurrentData =="author":
+                print "Author: ",self.author 
+            cur.execute("INSERT INTO inproceedings(pubkey, title,booktitle, year) VALUES(%s,%s,%s,%s)", (self.pubkey,self.title, self.booktitle, self.year))
 
-        if tag == "authorship":
-            print "***authorship***"
-            pubkey = attributes["key"]
-            print "Pubkey:",pubkey
+        #if tag == "authorship":
+        #    print "***authorship***"
+        #    pubkey = attributes["key"]
+        #    print "Pubkey:",pubkey
 
     def endElement(self, tag):
         if self.CurrentData == "title":
@@ -39,13 +64,16 @@ class DblpHandler(xml.sax.ContentHandler):
             print "Journal: ",self.journal
         elif self.CurrentData =="author":
             print "Author: ",self.author 
+            cur.execute("INSERT INTO authorship(pubkey, author) VALUES(%s, %s)", (self.pubkey , self.author))
+            #print pubkey, self.title, self.journal, self.year
+        #cur.execute("INSERT INTO article(pubkey, title,journal, year) VALUES(%s,%s,%s,%s)", (self.pubkey,self.title, self.journal, self.year))
         self.CurrentData = ""
 
     def characters(self, content):
         if self.CurrentData == "title":
             self.title= content
         elif self.CurrentData == "year":
-            self.year = content
+            self.year = int(content)
         elif self.CurrentData =="booktitle":
             self.booktitle = content
         elif self.CurrentData =="journal":
@@ -53,7 +81,38 @@ class DblpHandler(xml.sax.ContentHandler):
         elif self.CurrentData =="author":
             self.author = content
 if (__name__ == "__main__"):
+    #Try to connect
+    try:
+        conn = psycopg2.connect(dbname = 'dblptest', user = 'dblpuser', password = 'password')
+        conn.autocommit = True
+    except:
+        print "Unable to connect the database"
+        exit()
+    print "Successfully connected to the databese"
+    
+    #cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur = conn.cursor()
+    try: 
+        cur.execute("DROP TABLE IF EXISTS authorship")
+        cur.execute("CREATE TABLE IF NOT EXISTS authorship(pubkey varchar(255),author varchar(255))")
+    except:
+        print "Unable to create Table -- Authorship"
+    
+    try:
+        cur.execute("DROP TABLE IF EXISTS inproceedings")
+        cur.execute("CREATE TABLE IF NOT EXISTS inproceedings (pubkey varchar(255),title varchar(255),booktitle varchar(255),year int)")
+    except:
+        print "Unable to create table -- inproceedings"
+
+    try:
+        cur.execute("DROP TABLE IF EXISTS article")
+        cur.execute("CREATE TABLE IF NOT EXISTS article(pubkey varchar(255),title varchar(511),journal varchar(255),year int)")
+    except:
+        print "Unable to create table -- article"
+    #conn.commit()
+    print "Done Creating Tables"
     # create an XMLReader
+
     parser = xml.sax.make_parser()
     # turn off nam epsaces
     parser.setFeature(xml.sax.handler.feature_namespaces, 0)
